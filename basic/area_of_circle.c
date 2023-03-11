7af2ff21b07be8a9c4415974cf6f6b4c5ada62d3
Ar12agnik <102238499+Ar12agnik@users.noreply.github.com>
2023-03-11 23:13:11 +0530

    update

---
+++ basic/area_of_circle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a,area;
+    printf("enter the radius: ");
+    scanf("%d",&a);
+    area=(22/7)*a*a;
+    printf("the area of the circle is %d",area);
+    return 0;
+}
+
